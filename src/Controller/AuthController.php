af1c758a99b21bf8732fa0c824e2c6b525cbd023
Lefevre-Alexandre <jean.lefevre.alexandre@gmail.com>
2020-10-18 10:46:07 +0400

    création des controllers

---
+++ src/Controller/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthController extends AbstractController
+{
+    /**
+     * @Route("/auth", name="auth")
+     */
+    public function index()
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/AuthController.php',
+        ]);
+    }
+}
