baa4107ea31622fa1466a05093a020b30ae13133
Lefevre-Alexandre <jean.lefevre.alexandre@gmail.com>
2020-10-18 11:01:18 +0400

    création des controller, fonctions requis et routes + package server dev env

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,17 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
 
 class UserController extends AbstractController
 {
+    
     /**
-     * @Route("/user", name="user")
+     * @Route("/api/v1/user/{id}", name="user-show")
      */
-    public function index()
+    public function show( $id )
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/UserController.php',
+            'user-id' => $id
         ]);
     }
 }
