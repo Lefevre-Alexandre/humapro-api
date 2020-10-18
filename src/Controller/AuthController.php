9bc7c6c159d52c8395f87f2a8517cd7eaf0dcd1c
Lefevre-Alexandre <jean.lefevre.alexandre@gmail.com>
2020-10-18 12:43:23 +0400

    implementation UserController ressource renvoie information utilisateur, AuthController ressource login et logout, création requete pour vérification de token et vérifications identité UserRepository

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class AuthController extends AbstractController
 {
@@ -12,33 +13,93 @@ class AuthController extends AbstractController
     /**
      * @Route("/api/v1/auth", name="token-auth")
      */
-    public function auth()
+    public function auth(Request $request)
     {
+        //récupération des paramétres
+        $email      = $request->query->get('email');
+        $password   = $request->query->get('password');
+        
+        //vérification des paramétres non vide
+        if( empty( $email ) || empty( $password ) ) {
+            return $this->json([
+                'message' => 'Paramétres manquants'              
+            ]);
+        }
+
+        //récupération de l'utilisateur celon les paramétres
+        $user_available = $this->getDoctrine()
+        ->getRepository(User::class)
+        ->checkUserAvailable($email, $password);
+
+        //vérifie si l'utilisateur est bien un utilisateur enregistrée
+        if( $user_available ) {
+            return $this->json([
+                'message' => 'Token a retourner'              
+            ]);
+        }
+
+        //le cas échéant si l'utilisateur n'est pas un utilisateur enregistré.
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AuthController.php',
+            'message' => 'Utilisateur non connu'              
         ]);
     }
 
     /**
      * @Route("/api/v1/logout", name="logout")
      */
-    public function logout()
+    public function logout(Request $request)
     {
+        //récupération des paramétres requis
+        $token  = $request->query->get('token');
+        //récupération de l'utilisateur lier au token
+        $user   = $this->checkToken( $token );
+
+        //vérification que le token est bien attribuer à un utilisateur l'utilisateur
+        if( $user ){
+
+            //reset du token
+            //$user->setAccessToken('');
+
+            return $this->json([
+                'message' => 'You get logout'     
+            ]);
+        }
+
+        //cas échéant si le token n'est pas lier à aucun utilisateur
         return $this->json([
-            'message' => 'Welcome to logout!',
-            'path' => 'src/Controller/AuthController.php',
+            'message' => 'No access logout'     
         ]);
+
     }
 
+    /**
+     * Génére un token pour les demande de token
+     */
     public function getToken()
     {
+        
+        $token_random = bin2hex( openssl_random_pseudo_bytes(16) );
 
+        //vérification que le token n'est pas déjà attribuer
+        while( $this->checkToken( $token_random ) ){
+            $token_random = bin2hex( openssl_random_pseudo_bytes(16) );
+        }
+
+        return $token_random;
+  
     }
 
-    public function checkToken()
+    /**
+     * Vérifie si le token est déjà attribuer à un utilisateur
+     */
+    public function checkToken( $token )
     {
         
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findUserWithToken($token);
+        
+            return $user;
     }
 
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends AbstractController
 {
@@ -12,12 +13,15 @@ class UserController extends AbstractController
     /**
      * @Route("/api/v1/user/{id}", name="user-show")
      */
-    public function show( $id )
-    {
+    public function show(Request $request, $id )
+    {   
+        //retireve user by id
+        $user = $this->getDoctrine()->getRepository(User::class)->find($id);
+        
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-            'user-id' => $id
+            'response'  => 200,
+            'nom'       => $user->getNom(),
+            'prenom'    => $user->getPrenom()
         ]);
     }
 }
